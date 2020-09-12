3e68550c4084a73aa5a140d9d6ab08de48ce6f12
Graeme Lockley <graemel@no9.co.za>
2020-09-12 15:26:33 +0200

    chore: upgrade viz command

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
-import * as CLI from "https://raw.githubusercontent.com/littlelanguages/deno-lib-console-cli/0.0.5/mod.ts";
+import * as CLI from "https://raw.githubusercontent.com/littlelanguages/deno-lib-console-cli/0.1.0/mod.ts";
 
 import { denoCommand } from "https://raw.githubusercontent.com/littlelanguages/scanpiler-tool-deno/0.1.0/mod.ts";
-import { vizCommand } from "https://raw.githubusercontent.com/littlelanguages/scanpiler-tool-viz/0.0.3/mod.ts";
+import { vizCommand } from "https://raw.githubusercontent.com/littlelanguages/scanpiler-tool-viz/0.1.0/mod.ts";
 
 const denoCmd = new CLI.ValueCommand(
   "deno",
