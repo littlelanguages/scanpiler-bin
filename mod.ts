e800369445fecd5ac426645c3829109c8f1a529f
Graeme Lockley <graemel@no9.co.za>
2020-10-10 07:26:57 +0200

    feature: integrate kotlin command

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import * as CLI from "https://raw.githubusercontent.com/littlelanguages/deno-lib-console-cli/0.1.1/mod.ts";
 
 import { denoCommand } from "https://raw.githubusercontent.com/littlelanguages/scanpiler-tool-deno/0.2.2/mod.ts";
+import { command as kotlinCommand } from "https://raw.githubusercontent.com/littlelanguages/scanpiler-tool-kotlin/0.0.2/mod.ts";
 import { vizCommand } from "https://raw.githubusercontent.com/littlelanguages/scanpiler-tool-viz/0.1.1/mod.ts";
 
 const denoCmd = new CLI.ValueCommand(
@@ -23,8 +24,7 @@ const denoCmd = new CLI.ValueCommand(
   {
     name: "FileName",
     optional: false,
-    help:
-      "The name of the file the is to be processed.  If the name does not end with .ll then .ll is appended to the name.",
+    help: "The name of the file the is to be processed.",
   },
   (
     _: CLI.Definition,
@@ -35,6 +35,49 @@ const denoCmd = new CLI.ValueCommand(
       file!,
       {
         directory: vals.get("directory") as string | undefined,
+        force: vals.get("force") as boolean | false,
+        verbose: vals.get("verbose") as boolean | false,
+      },
+    );
+  },
+);
+
+const kotlinCmd = new CLI.ValueCommand(
+  "kotlin",
+  "Create a scanner for the passed lexical definition in Kotlin",
+  [
+    new CLI.ValueOption(
+      ["--directory", "-d"],
+      "Value is the directory into which the generated Kotlin and library code is placed.  Defaults to the source file's directory.",
+    ),
+    new CLI.ValueOption(
+      ["--package", "-p"],
+      "Value is the package name into which the generated Kotlin code is to be placed.",
+    ),
+    new CLI.FlagOption(
+      ["--force", "-f"],
+      "Ignore all the file dates and force a regeneration of all generated sources.",
+    ),
+    new CLI.FlagOption(
+      ["--verbose", "-v"],
+      "Lists all the files as they are created.",
+    ),
+  ],
+  {
+    name: "FileName",
+    optional: false,
+    help: "The name of the file the is to be processed.",
+  },
+  (
+    _: CLI.Definition,
+    file: string | undefined,
+    vals: Map<String, unknown>,
+  ) => {
+    kotlinCommand(
+      file!,
+      {
+        directory: vals.get("directory") as string | undefined,
+        name: vals.get("package") as string,
         force: vals.get("force") as boolean | false,
         verbose: vals.get("verbose") as boolean | false,
       },
@@ -54,8 +97,7 @@ const vizCmd = new CLI.ValueCommand(
   {
     name: "FileName",
     optional: false,
-    help:
-      "The name of the file the is to be processed.  If the name does not end with .ll then .ll is appended to the name.",
+    help: "The name of the file the is to be processed.",
   },
   (
     _: CLI.Definition,
@@ -73,7 +115,7 @@ const cli = {
   name: "scanpiler",
   help: "Validate and compile a scanner definition into executable code",
   options: [CLI.helpFlag],
-  cmds: [denoCmd, vizCmd, CLI.helpCmd],
+  cmds: [denoCmd, kotlinCmd, vizCmd, CLI.helpCmd],
 };
 
 CLI.process(cli);
